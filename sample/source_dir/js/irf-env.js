24f39779e13dc243c8ce0a5e597e7bba1fa36340
Stalin Gino <stalingino@gmail.com>
2021-04-21 11:22:34 +0530

    Update irf-env.js

--- sample/source_dir/js/irf-env.js
+++ sample/source_dir/js/irf-env.js
@@ -1,6 +1,11 @@
 var irf = irf || {};
-irf.BASE_URL = '{{sample.api}}';
+irf.BASE_URL = '{{perdix.api}}';
 irf.MANAGEMENT_BASE_URL = '{{management.base}}';
 irf.FORM_DOWNLOAD_URL = '{{forms.jsp}}';
-// {{sample.database.username}}
-// {{sample.database.password}}
+irf.BI_BASE_URL = "{{bi.base}}";
+irf.PSYCHOMETRIC_BASE_URL = "{{psychometric.api}}";
+irf.AUDIT_API_URL = '{{audit.api}}';
+irf.ANALYTICS_WEB_URL = '{{analytics.web}}';
+irf.ANALYTICS_API_URL = '{{analytics.api}}';
+// {{perdix.database.username}}
+// {{audit.datasource.password}}
